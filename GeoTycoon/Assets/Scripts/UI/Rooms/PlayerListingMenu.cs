c31103dd404e3bb8d376a70cf198063f478c6b8e
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2024-05-30 17:29:04 +0700

    Fix on correct User

--- GeoTycoon/Assets/Scripts/UI/Rooms/PlayerListingMenu.cs
+++ GeoTycoon/Assets/Scripts/UI/Rooms/PlayerListingMenu.cs
@@ -3,6 +3,7 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerListingMenu : MonoBehaviourPunCallbacks
 {
@@ -10,27 +11,57 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
     private Transform _content;
     [SerializeField]
     private PlayerListing _playerlisting;
+    [SerializeField]
+    private Text _readyUpText;
 
     private List<PlayerListing> _listings = new List<PlayerListing>();
     private RoomsCanvases _roomsCanvases;
+    private bool _ready = false;
 
-    private void Awake()
+
+    public override void OnEnable()
     {
+        base.OnEnable();
+        SetReadyUp(false);
         GetCurrentRoomPlayers();
     }
 
+    public override void OnDisable() 
+    { 
+        base.OnDisable();
+        for (int i = 0; i < _listings.Count; i++)
+            Destroy(_listings[i].gameObject);
+        _listings.Clear();
+    }
     public void FirstInitialize(RoomsCanvases canvases)
     {
         _roomsCanvases = canvases;
     }
 
-    public override void OnLeftRoom()
+    private void SetReadyUp(bool state)
     {
-        _content.DestroyChildren();
+        _ready = state;
+        if (state)
+        {
+            if (_ready)
+            {
+                _readyUpText.text = "R";
+            }
+            else
+            {
+                _readyUpText.text = "N";
+            }
+        }
+
     }
 
     private void GetCurrentRoomPlayers()
     {
+        if (!PhotonNetwork.IsConnected)
+            return;
+        if (PhotonNetwork.CurrentRoom == null || PhotonNetwork.CurrentRoom.Players == null)
+            return;
+
         foreach (KeyValuePair<int, Player> playerInfo in PhotonNetwork.CurrentRoom.Players)
         {
             AddPlayerListing(playerInfo.Value);
@@ -40,12 +71,26 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
 
     private void AddPlayerListing(Player player)
     {
-        PlayerListing listing = Instantiate(_playerlisting, _content);
-        if (listing != null)
+        int index = _listings.FindIndex(x => x.Player == player);
+        if (index!= -1)
         {
-            listing.SetPlayerInfo(player);
-            _listings.Add(listing);
+            _listings[index].SetPlayerInfo(player);
         }
+        else
+        {
+            PlayerListing listing = Instantiate(_playerlisting, _content);
+            if (listing != null)
+            {
+                listing.SetPlayerInfo(player);
+                _listings.Add(listing);
+            }
+        }
+        
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        _roomsCanvases.CurrentRoomCanva.LeaveRoomMenu.OnClick_LeaveRoom();
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
