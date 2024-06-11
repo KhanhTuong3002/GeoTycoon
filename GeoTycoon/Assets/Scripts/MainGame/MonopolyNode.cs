e112ed5c3b5ea5338af7a335b49717513d259379
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2024-06-11 09:02:08 +0700

    Player Class and Infor

--- GeoTycoon/Assets/Scripts/MainGame/MonopolyNode.cs
+++ GeoTycoon/Assets/Scripts/MainGame/MonopolyNode.cs
@@ -22,6 +22,22 @@ public class MonopolyNode : MonoBehaviour
     [SerializeField] internal new string name;
     public MonopolyNodeType monopolyNodeType;
     [SerializeField] TMP_Text nameText;
+    [Header("Property Price")]
+    public int price;
+    [SerializeField] TMP_Text priceText;
+    [Header("Property Rent")]
+    [SerializeField] bool calculateRentAuto;
+    [SerializeField] int currentRent;
+    [SerializeField] internal int baseRent;
+    [SerializeField] internal int[] rentWithHouse;
+    [Header("Property Mortgage")]
+    [SerializeField] GameObject morgtgageImage;
+    [SerializeField] GameObject propertyImage;
+    [SerializeField] bool isMortgaged;
+    [SerializeField] int mortgageValue;
+    [Header("Preperty Owner")]
+    [SerializeField] GameObject ownerBar;
+    [SerializeField] TMP_Text ownerText;
 
     private void OnValidate()
     {
@@ -29,5 +45,78 @@ public class MonopolyNode : MonoBehaviour
         {
             nameText.text = name;
         }
+        //CALCULATION
+        if(calculateRentAuto)
+        {
+            if(monopolyNodeType == MonopolyNodeType.Property)
+            {
+                if(baseRent > 0) 
+                {
+                    price = 3 * (baseRent * 10);
+                    //MORTGAGE PRICE
+                    mortgageValue = price / 2;
+                    rentWithHouse = new int[]
+                    {
+                        baseRent * 5,
+                        baseRent * 5 * 3,
+                        baseRent * 5 * 9,
+                        baseRent * 5 * 16,
+                        baseRent * 5 * 25,
+                    };
+                }
+            }
+            if (monopolyNodeType == MonopolyNodeType.Railroad)
+            {
+                mortgageValue = price / 2;
+            }
+            if (monopolyNodeType == MonopolyNodeType.Utility)
+            {
+                mortgageValue = price / 2;
+            }
+        }
+        if (priceText != null)
+        {
+            priceText.text = "$ " + price;
+        }
+        //UPDATE THE OWNER
+
     }
+    // MONRTGAGE CONTENT
+    public int MortagageProperty()
+    {
+        isMortgaged = true;
+        morgtgageImage.SetActive(true);
+        propertyImage.SetActive(false);
+        return mortgageValue;
+    }
+
+    public void UnMortgageProperty()
+    {
+        isMortgaged = false;
+        morgtgageImage.SetActive(false);
+        propertyImage.SetActive(true);
+    }
+
+    public bool IsMortgaged => isMortgaged;
+    public int MortgageValue => mortgageValue;
+
+    //UPADTE OWNER
+    public void OnOwnerUpdate () 
+    {
+        if (ownerBar != null)
+        {
+            if(ownerText.text != "")
+            {
+                ownerBar.SetActive(true);
+                //ownerText.text = ownerBar.name;
+            }
+            else
+            {
+                ownerBar.SetActive(false);
+                ownerText.text = "";
+            }
+        }
+    }
+      
+    
 }
---
+++ GeoTycoon/Assets/Scripts/MainGame/Player_Mono.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+[System.Serializable]
+public class Player_Mono
+{
+    public enum PlayerType
+    {
+        HUMAN,
+        AI
+    }//HUMAN
+    public PlayerType playerType;
+    public string name;
+    int money;
+    MonopolyNode currentnode;
+    bool isInjail;
+    int numTurnsInJail;
+    [SerializeField] GameObject myTonken;
+    [SerializeField] List<MonopolyNode> myMonopolyNodes = new List<MonopolyNode>();
+
+    //PLAYERINFOR
+    Player_MonoInfor myInfor;
+
+
+    //AI
+    int aiMoneySavity = 200;
+
+
+    //RETURN SOME INFORS
+    public bool IsInjail => isInjail;
+    public GameObject MyTonken => myTonken;
+    public MonopolyNode MyMonopolyNode => currentnode;
+
+    public void Inititialize(MonopolyNode startNode, int startMoney, Player_MonoInfor info)
+    {
+        currentnode = startNode;
+        money = startMoney;
+        myInfor = info;
+        myInfor.SetPlayerNameandCash(money, name);
+    }
+
+
+
+
+
+
+}
