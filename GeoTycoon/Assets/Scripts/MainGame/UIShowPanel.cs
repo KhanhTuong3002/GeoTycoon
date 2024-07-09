6bc56301904beed3943a090153638f6650935b44
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2024-07-09 09:50:06 +0700

    update name

--- GeoTycoon/Assets/Scripts/MainGame/UIShowPanel.cs
+++ GeoTycoon/Assets/Scripts/MainGame/UIShowPanel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UIShowPanel : MonoBehaviour
 {
     [SerializeField] GameObject humanPanel;
     [SerializeField] Button rollDiceButton;
